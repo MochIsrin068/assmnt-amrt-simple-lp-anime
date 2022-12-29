cd6e409550335add50e01cf3432341c777b0ccd9
MochIsrin068 <isrin068@gmail.com>
2022-12-29 22:41:35 +0800

    remove chakra ui : minimalize third party

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { ColorModeSwitcher } from "../../commons/components/ColorModeSwitcher";
 
 export default function Home() {
   return (
     <div>
-      <ColorModeSwitcher />
       <h2>Home</h2>
     </div>
   );
